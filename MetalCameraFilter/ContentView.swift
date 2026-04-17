bf107f74faacd46dd46dfbe664c6909bf84afd01
taka-sakamoto <goodspeed855@gmail.com>
2026-04-17 13:19:23 +0900

    Initial Commit

---
+++ MetalCameraFilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalCameraFilter
+//
+//  Created by Takayuki Sakamoto on 2026/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalCameraFilter/MetalCameraFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalCameraFilterApp.swift
+//  MetalCameraFilter
+//
+//  Created by Takayuki Sakamoto on 2026/04/17.
+//
+
+import SwiftUI
+
+@main
+struct MetalCameraFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
